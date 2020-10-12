962e3fab12d04d5a7851d1dc46fb4ca63dbeb51d
Chelsey <cswilik@gmail.com>
2020-10-12 18:52:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,10 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |calls|
-    valid_calls.include?
+    valid_calls.include?(calls)
+  end 
+end 
+
 
  
 
